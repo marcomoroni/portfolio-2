81759d2d9a94ff64add933d9cf7b256175c65823
Marco Moroni <marcomoroni394@gmail.com>
2021-10-24 15:10:49 +0100

    Typewriter

---
+++ static/script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const typewriterTimeline = [
+    {
+        _kind: 'wait',
+        duration: 1600,
+    },
+    {
+        _kind: 'type',
+        content: ', I\'m Marco :)',
+        perLetterDuration: 170,
+    },
+    {
+        _kind: 'wait',
+        duration: 2300,
+    },
+    {
+        _kind: 'delete',
+        count: 3,
+        perLetterDuration: 50,
+    },
+    {
+        _kind: 'wait',
+        duration: 600,
+    },
+    {
+        _kind: 'insert',
+        content: '.',
+    },
+];
+
+window.addEventListener('load', () => {
+    // Typewriter.
+    const typewriterEl = document.getElementById('typewriter');
+    if (typewriterEl != null) {
+        let currentDelay = 0;
+        typewriterTimeline.forEach((x) => {
+            switch(x._kind) {
+                case 'wait':
+                    currentDelay += x.duration;
+                    break;
+                case 'type':
+                    for(let i = 0; i < x.content.length; i++) {
+                        setTimeout(() => {
+                            typewriterEl.innerHTML += x.content.charAt(i);
+                        }, currentDelay + x.perLetterDuration * i);
+                    }
+                    currentDelay += x.perLetterDuration * (x.content.length - 1);
+                    break;
+                case 'insert':
+                    setTimeout(() => {
+                        typewriterEl.innerHTML += x.content;
+                    }, currentDelay);
+                    break;
+                case 'delete':
+                    for(let i = 0; i < x.count; i++) {
+                        setTimeout(() => {
+                            typewriterEl.innerHTML = typewriterEl.innerHTML.slice(0, -1);
+                        }, currentDelay + x.perLetterDuration * i);
+                        currentDelay += x.perLetterDuration * (x.count - 1);
+                    }
+                    break;
+            }
+        });
+    }
+});
